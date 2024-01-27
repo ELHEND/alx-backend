ae0675d73c8e6659d752225d8871355437e073cc
ELHEND <alhnd683@gmail.com>
2024-01-26 22:55:38 -0800

    hndaia

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """
 Simple helper function named index_range
+
 takes two arguments page and page_size
-Returns a tuple of size two (start index, end index)
-corresponding to the range of indexes to return in a list
+
+Returns  tuple of size two (start index, end index)
+
+corresponding to  range of indexes to return in a list
+
 for those particular pagination parameters
 """
 from typing import Tuple
@@ -12,7 +16,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Prototype: def index_range(page:int, page_size: int)
-    Returns a Tuple of size
+    Returns Tuple of size
     """
     index = page * page_size - page_size
     index_ = index + page_size
