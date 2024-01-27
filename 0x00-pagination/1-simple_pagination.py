b11181e5ef8d02f8af4827d65cadb221f2b138c9
ELHEND <alhnd683@gmail.com>
2024-01-26 22:53:46 -0800

    hnoda

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -2,11 +2,13 @@
 """Simple pagination sample.
 """
 import csv
+
+
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
+    """Retrieves index range from  given page and page size.
     """
     start = (page - 1) * page_size
     end = start + page_size
@@ -14,12 +16,12 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """Server class to paginate  database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance.
+        """Initializes new Server instance.
         """
         self.__dataset = None
 
@@ -35,7 +37,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
+        """Retrieves  page of data.
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
